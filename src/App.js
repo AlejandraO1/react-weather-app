f9607709973726655b444053b021efa2df1fbf2d
AlejandraO1 <alejandraortegax@gmail.com>
2023-02-22 12:50:32 -0500

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by <strong>Alejandra Ortega</strong> and is{" "}
+        <a
+          href="https://github.com/AlejandraO1/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
 
 export default App;

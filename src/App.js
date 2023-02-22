c558f20b0dba7a088b915c33cb568ecc989a2782
AlejandraO1 <alejandraortegax@gmail.com>
2023-02-22 13:30:03 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This project was coded by <strong>Alejandra Ortega</strong> and is{" "}
-        <a
-          href="https://github.com/AlejandraO1/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project was coded by <strong>Alejandra Ortega</strong> and is{" "}
+          <a
+            href="https://github.com/AlejandraO1/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }

797316d7c7e59d8a46f4eb43e3613899c435754a
AlejandraO1 <alejandraortegax@gmail.com>
2023-02-23 16:03:46 -0500

    Edited styling for overall app

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,25 +5,27 @@ import TemperatureConversion from "./TemperatureConversion.js";
 
 export default function WeatherInfo(props) {
   return (
-    <div>
-      <h1>{props.data.city}</h1>
-      <p>
-        <FormattedDate date={props.data.date} />
-      </p>
-      <ul>
-        <li>
-          <WeatherIcon code={props.data.icon} />
-        </li>
-        <li>
-          <TemperatureConversion fahrenheit={props.data.temperature} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          Humidity: {Math.round(props.data.humidity)}%
+    <div className="backgroundWeather">
+      <div className="displayWeather">
+        <h1>{props.data.city}</h1>
+        <p>
+          <FormattedDate date={props.data.date} />
+        </p>
+        <ul>
+          <li>
+            <WeatherIcon code={props.data.icon} />
+          </li>
+          <li>
+            <TemperatureConversion fahrenheit={props.data.temperature} />
+          </li>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            Humidity: {Math.round(props.data.humidity)}%
+          </div>
+          <div className="col-6">Wind: {Math.round(props.data.wind)}km/h </div>
         </div>
-        <div className="col-6">Wind: {Math.round(props.data.wind)}km/h </div>
       </div>
     </div>
   );
